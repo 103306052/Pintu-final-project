6bb99c9234ccf107d506cb6baf543f1a9f77134a
103306052 <103306052@nccu.edu.tw>
2016-12-25 20:52:50 +0800

    district
    distribution complete(exception isn't finished yet)

--- district.php
+++ district.php
@@ -5,6 +5,15 @@
     <title></title>
   </head>
   <body>
+    <form action="district.php" method="post" id="distribute">
+      <h>輸入權重：</h>
+      <br>
+      北部 (建議值0.4)：<input type="float" name="north" value="" />
+      中部 (建議值0.3)：<input type="float" name="mid" value="" />
+      南部 (建議值0.3)：<input type="float" name="south" value="" />
+      <input type="submit">
+    </form>
+    <br/><br/>
     <?php
      error_reporting(~E_DEPRECATED & ~E_NOTICE);
 
@@ -27,16 +36,24 @@
 
     $sql = "select * from products";//查詢整個表單
     $result = mysql_query($sql) ;
+
     while ($row = mysql_fetch_array($result)) {
-        echo "商品編號 : ".$row['id']."<br>";
-        echo "商品名稱 : ".$row['name']."<br>";
+        echo "商品編號 : ".$row['id']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+        echo "商品名稱 : ".$row['name']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
         if ($row['EOQ'] == true) {
-            echo "EOQ : ".$row['EOQ']."<br>";
+            echo "EOQ : ".$row['EOQ']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "北部 : ".round($row['EOQ']*$_POST["north"])."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "中部 : ".round($row['EOQ']*$_POST["mid"])."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "南部 : ".round($row['EOQ']*$_POST["south"])."<br>";
         } else {
-            echo "EPQ : ".$row['EPQ']."<br>";
+            echo "EPQ : ".$row['EPQ']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "北部 : ".round($row['EPQ']*$_POST["north"])."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "中部 : ".round($row['EPQ']*$_POST["mid"])."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+            echo "南部 : ".round($row['EPQ']*$_POST["south"])."<br>";
         }
         echo "<br>";
     }//顯示資料
     ?>
+
   </body>
 </html>
