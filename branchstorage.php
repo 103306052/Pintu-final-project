cf69c581740250d5f850c672afa5cd73956877f3
103306052 <103306052@nccu.edu.tw>
2017-01-01 21:30:51 +0800

    update
    仍然試不出別種方法，暫且只能用id查詢方式，進貨功能也有問題無法update，求解QQ

--- branchstorage.php
+++ branchstorage.php
@@ -28,17 +28,26 @@
           die("Database Connection failed : " . mysql_error());
       }
       //以上為資料庫連結部分
-     $sql = "select * from shop where id =".$_POST["shop_id"];//查詢整個表單
+     $sql = "select * from shop where id =".$_POST["shop_id"];
+     //查詢整個表單
      $result = mysql_query($sql);
-
-     while ($row = mysql_fetch_array($result)) {
+     while ($row = @mysql_fetch_array($result)) {
          echo "<br>";
          echo "店名：".$row['name']."&nbsp"."&nbsp"."&nbsp"."&nbsp"."<br>";
          echo "產品名稱：".$row['products_name']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
          echo "剩餘存貨：".$row['products_Q']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+         //顯示商店存貨資料
+          ?>
+         <form action='branchstorage.php' method = 'post'>
+           <input type="submit" name="inventory_update" value="進貨">
+         </form>
+         <?php
+            if (isset($_POST['inventory_update'])) {
+                $increase = "update shop set products_Q  = products_Q + products_distributed_Q where id =".$_POST["shop_id"];
+                mysql_query($increase);
+            }
      }
-
-
+     //進貨功能（目前無效）
     ?>
   </body>
 </html>
