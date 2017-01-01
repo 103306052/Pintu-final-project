09457fb2bd038086e62cc6f3dba3c1adfabb4bda
Alice1227 <a23305760@yahoo.com.tw>
2017-01-02 00:25:36 +0800

    加了require

--- marketing.php
+++ marketing.php
@@ -6,21 +6,7 @@
 </head>
 <body>
   <?php
-  error_reporting(~E_DEPRECATED & ~E_NOTICE);
-  define('DBHOST', 'localhost');
-  define('DBUSER', 'root');
-  define('DBPASS', '103306018');
-  define('DBNAME', 'pintu');
-  $conn = mysql_connect(DBHOST, DBUSER, DBPASS);
-  $dbcon = mysql_select_db(DBNAME);
-  if (!$conn) {
-    die("Connection failed : " . mysql_error());
-  }
-  if (!$dbcon) {
-    die("Database Connection failed : " . mysql_error());
-  }
-
-  mysql_set_charset('utf8', $conn);
+  require "db.php";
 
   function printTable($sql){
     $result = mysql_query($sql);
