e28bc5d9b797df0246241d05d6de38d850c4dfea
103306052 <103306052@nccu.edu.tw>
2017-01-01 22:59:30 +0800

    進貨功能php檔

---
+++ update.php
@@ -0,0 +1,28 @@
+<?php
+
+error_reporting(~E_DEPRECATED & ~E_NOTICE);
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '5453489j');
+define('DBNAME', 'pintu');
+
+$conn = mysql_connect(DBHOST, DBUSER, DBPASS);
+$dbcon = mysql_select_db(DBNAME);
+
+if (!$conn) {
+    die("Connection failed : " . mysql_error());
+}
+
+if (!$dbcon) {
+    die("Database Connection failed : " . mysql_error());
+}
+//以上為資料庫連結部分
+$query = "update shop set products_Q  = products_Q + products_distributed_Q where id ={$_POST['id']}";
+mysql_query($query);
+?>
+<script>
+alert("進貨成功！")
+history.go(-1);
+location.reload();
+</script>
