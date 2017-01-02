b57add6b6db113a1206412e85fdebebce79fc6e0
103306052 <103306052@nccu.edu.tw>
2017-01-02 23:56:58 +0800

    連好後端了(只剩下底下背景要淡化處理)

--- branchstorage.php
+++ branchstorage.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title></title>
+    <meta charset="UTF-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/branchstorage1Css.css">
+		<title>Branchstorage</title>
+		<style>
+			body{
+				margin:0;
+				padding:0;
+				background: #000 url(img/background.jpg) center center fixed no-repeat;
+				-moz-background-size: cover;
+				background-size: cover;
+			}
+		</style>
   </head>
   <body>
-    <form action="branchstorage.php" method="post" id="shop">
-      輸入分店ID：<input type="int" name="shop_id" value="" />
-      <input type="submit" value="登入">
-    </form>
+    <div class="banner">
+			<img src="img/branchstorage/banner.jpg" alt="">
+		</div>
+		<br>
+		<div class="container">
+			<div class="row">
+				<div class="signin">
+					<form class="form-inline" action="branchstorage.php" method="post" id="shop" role="form">
+						<input type="int" name="shop_id" value="" class="form-control" placeholder="請輸入分店ID">
+						<input type="submit" class="btn btn-default" value="登入">
+					</form>
+				</div>
+			</div>
+		</div>
+
     <?php
       error_reporting(~E_DEPRECATED & ~E_NOTICE);
 
@@ -33,21 +56,30 @@
      //查詢整個表單
      $result = mysql_query($sql);
      while ($row = @mysql_fetch_array($result)) {
-         echo "<br>";
-         echo "店名：".$row['name']."&nbsp"."&nbsp"."&nbsp"."&nbsp"."<br>";
-         echo "產品名稱：".$row['products_name']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
-         echo "剩餘存貨：".$row['products_Q']."&nbsp"."&nbsp"."&nbsp"."&nbsp";
-         //顯示商店存貨資料
-
-
-          ?>
-          <td class="contact-update">
-            <form action='update.php?id="<?php echo $id; ?>"' method="post">
-              <input type="hidden" name="id" value="<?php echo $id; ?>">
-              <input type="submit" name="submit" value="進貨">
-              <Input Type="Button" Value="重新整理" onClick="window.location.reload();">
-            </form>
-          </td>
+         ?>
+          <div class="storage">
+            <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <h4>商品名稱:</h4>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <h4><?php echo $row['products_name'] ?></h4>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <h4>剩餘數量:</h4>
+            </div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+              <h4><?php echo  $row['products_Q'] ?></h4>
+            </div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+              <form action='update.php?id="<?php echo $id; ?>"' method="post">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <input type="submit" name="submit" value="進貨" class="btn btn-default">
+              </form>
+            </div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+              <input type="button" value="重新整理" onClick="window.location.reload();" class="btn btn-default">
+            </div>
+          </div>
           <?php
 
      } ?>
